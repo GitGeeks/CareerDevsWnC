1af6a097a9f0795b3e7f718d216589bb371ca4a4
Arnell Milhouse <arnell@intracitygeeks.org>
2017-07-13 04:41:50 -0400

    version1-Arrays complete

--- todos.js
+++ todos.js
@@ -1,1 +1,48 @@
 // begin
+// Version 1 - Arrays -- requirements
+// it should have a way to store todos
+
+
+
+
+
+var todos = ['item 1', 'item 2', 'item 3']
+
+// it should have a way to display todos
+
+
+
+
+
+var todos = ['item 1', 'item 2', 'item 3']
+
+// it should have a way to add todos
+
+
+
+
+
+todos.push('item 4')
+todos.push('item 5')
+
+// it should have a way to change todos
+// todos[0]
+
+
+
+
+
+
+todos[0] = 'item 1 updated'
+todos
+
+// it should have a way to delete todos
+
+
+
+
+
+todos.splice(0,1)
+todos
+todos.splice(3,1)
+todos.splice(3,1)
